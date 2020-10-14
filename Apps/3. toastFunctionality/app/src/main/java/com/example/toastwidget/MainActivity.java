fbf8299add570a6cd2552d54727cd51e299c83a1
Sourav Ranjan Maharana <saurav.maharana07@gmail.com>
2020-10-14 19:28:21 +0530

    Added Toast Widget. App 4

---
+++ Apps/3. toastFunctionality/app/src/main/java/com/example/toastwidget/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toastwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText name = findViewById(R.id.editTextTextPersonName3);
+        Toast.makeText(this, "Hey There, " + name.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
